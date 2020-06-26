a40e8fe238f83259107adf715c68361e3b9ec0b0
Nehemias Herrera <yo@nehemiascr.com>
2020-06-26 14:18:47 +0000

    [add] reglas salariales de ccss, lpt y renta

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Costa Rica Payroll',
+    'category': 'Human Resources',
+    'author': 'The nice people at odoocr',
+    'depends': ['hr_payroll'],
+    'description': """
+Costa Rica Payroll Rules.
+=====================
+    - Configuration of hr_payroll for Costa Rica localization
+    - All main contributions rules for Costa Rica payslip'
+
+    """,
+    'data': [
+        'data/hr_salary_rule_data.xml',
+        'data/hr_payroll_structure_data.xml
+        '
+    ],
+}
