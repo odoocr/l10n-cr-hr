f9589dd05936336d3589228231b2775118e09ff6
Nehemias Herrera <yo@nehemiascr.com>
2020-06-26 14:56:57 +0000

    [fix] data filenames in manifest should not break lines

--- __manifest__.py
+++ __manifest__.py
@@ -13,7 +13,6 @@ Costa Rica Payroll Rules.
     """,
     'data': [
         'data/hr_salary_rule_data.xml',
-        'data/hr_payroll_structure_data.xml
-        '
+        'data/hr_payroll_structure_data.xml'
     ],
 }
